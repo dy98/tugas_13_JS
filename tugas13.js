d2636385e3bc3efd9d09f09103a83582de90c2e5
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 08:45:27 +0700

    tugas 13 JS
    tugas untuk Niomic

---
+++ tugas13.js
@@ -0,0 +1,12 @@
+function panggilBuah() {
+    let buah = ['pisang', 'jeruk'];
+
+    console.log(buah);
+
+    buah.unshift('apel', 'mangga');
+    console.log(buah);
+
+    return buah;
+}
+
+panggilBuah();
